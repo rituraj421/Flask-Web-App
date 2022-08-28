31baffe7af9188b9b8ae224bef9c5036a2b1d0ce
rituraj <riturajroj1234@gmail.com>
2022-08-28 12:00:57 +0530

    main content

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
-print(__name__)
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+ 
+app = Flask(__name__)
+app.config["SECRET_KEY"] = 'secret-key'
+app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///data.db'
+
+db = SQLAlchemy(app)
+#outside 
+@app.before_first_request
+def create_tables():
+    db.create_all()
+#end outside
+from routes import *
+
+if __name__ == '__main__':
+    app.run(debug=True)
